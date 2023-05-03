832ee6d4e48f5c8f6ab0ec4f659ba2760da79d35
1119-2916 <ti11192916@gmail.com>
2023-05-03 17:13:11 +0900

    分足取得まで実装

--- trade_simulator/trade_simulator/trade_simulator.py
+++ trade_simulator/trade_simulator/trade_simulator.py
@@ -1,19 +1,28 @@
 import csv
-import datetime
+from datetime import datetime, timedelta
 from .olhcv import OLHCV
 
 
 class TradeSimulator:
     def __init__(self, input_path: str = None) -> None:
-        if input_path is not None:
+        if input_path is not None:  # test fixture 導入までは引数 None を認める
             with open(input_path) as f:
                 reader = csv.reader(f)
                 self.chart = [row for row in reader]
+                self.chart.pop(0)  # 先頭行はカラム名なので除去
 
     def get_all_chart(self) -> list[list]:
-        pass
+        return self.chart
 
-    def _generate_OLHCV_from_cryptodatadownload_style_list(self, input: list) -> OLHCV:
+    def get_OLHCV(self, target_datetime: datetime) -> OLHCV:
+        for i in self.olhcv:
+            diff: timedelta = i.time - target_datetime
+            if abs(diff.total_seconds()) < 30.0:  # 1分足で取得することを想定している
+                return i
+        return None
+
+    @staticmethod
+    def generate_OLHCV_from_cryptodatadownload_style_list(input: list) -> OLHCV:
         unix = int(input[0])
         format = "%Y-%m-%d %H:%M:%S"
         dt = datetime.datetime.strptime(input[1], format)
@@ -23,6 +32,9 @@ class TradeSimulator:
         close = int(input[6])
         volume = int(input[7])
         return OLHCV(unix, dt, input[2], open, high, low, close, volume)
+
+    def get_all_OLHCV(self) -> list[OLHCV]:
+        return self.olhcv
 
     def get_chart_head(self, lines: int) -> list:
         return self.chart[:lines]
@@ -37,3 +49,6 @@ class TradeSimulator:
 
     def get_chart_line_count(self) -> int:
         return len(self.chart)
+
+    def _generate_OLHCV_list(self) -> None:
+        self.olhcv: list[OLHCV] = [self.generate_OLHCV_from_cryptodatadownload_style_list(i) for i in self.chart]
